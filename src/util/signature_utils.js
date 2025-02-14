334bded7e34a11884950f5d6d2b29539d7787539
Guy Margalit <guymguym@gmail.com>
2025-02-15 01:06:26 +0200

    GUY TEMP: allow empty amzdate ???

--- src/util/signature_utils.js
+++ src/util/signature_utils.js
@@ -43,7 +43,7 @@ function _authenticate_header_v4(req) {
     const service = credentials[3];
 
     // see http://docs.aws.amazon.com/general/latest/gr/sigv4-date-handling.html
-    if (!xamzdate.startsWith(credentials[1])) {
+    if (!xamzdate?.startsWith(credentials[1])) {
         throw new Error('Mismatching date in Authorization Credential and x-amz-date');
     }
 
@@ -78,7 +78,7 @@ function _authenticate_query_v4(req) {
     const service = credentials[3];
 
     // see http://docs.aws.amazon.com/general/latest/gr/sigv4-date-handling.html
-    if (!xamzdate.startsWith(credentials[1])) {
+    if (!xamzdate?.startsWith(credentials[1])) {
         throw new Error('Mismatching date in X-Amz-Credential and X-Amz-Date');
     }
 
